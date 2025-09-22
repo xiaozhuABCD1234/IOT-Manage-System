9dc87185fca1e0b798766c66aa7229a3b43d9cdc
xiaozhu <xiaozhuABCD1234@163.com>
2025-09-22 12:32:17 +0800

    refactor(ui): 更新 MarkManView 和 MarksStatusView 的模板结构
    在 MarkManView 的模板中移除了旧的 ResizablePanelGroup 结构，改为使用一个简单的 div 容器，并将 TypeGrid 替换为 DeviceCreate。
    
    在 MarksStatusView 的模板中，将旧的 DeviceCreate 组件嵌套在 ResizablePanel 中的结构进行修改，新增了一个垂直方向的 ResizablePanelGroup，并在其内部添加了 TypeGrid 和 TagGrid 组件。
    
    这些修改旨在优化两个视图的布局和功能，使其更符合新的设计需求。

--- frontend/src/views/MarkManView.vue
+++ frontend/src/views/MarkManView.vue
@@ -1,18 +1,10 @@
 <template>
-  <!-- 高度自己定，这里占满屏幕剩余区域 -->
-  <ResizablePanelGroup direction="horizontal" class="h-full w-full">
-    <ResizablePanel>
-      <TypeGrid />
-    </ResizablePanel>
-    <ResizableHandle with-handle />
-    <ResizablePanel>
-      <TagGrid />
-    </ResizablePanel>
-  </ResizablePanelGroup>
+  <div class="h-full w-full">
+    <DeviceCreate />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
-import TypeGrid from "@/components/TypeGrid.vue";
-import TagGrid from "@/components/TagGrid.vue";
+import DeviceCreate from "@/components/DeviceCreateForm.vue";
 </script>
--- frontend/src/views/MarksStatusView.vue
+++ frontend/src/views/MarksStatusView.vue
@@ -2,12 +2,23 @@
   <ResizablePanelGroup direction="horizontal" class="h-full w-full">
     <ResizablePanel> <MarksOnline /></ResizablePanel>
     <ResizableHandle with-handle />
-    <ResizablePanel> <DeviceCreate /></ResizablePanel>
+    <ResizablePanel>
+      <ResizablePanelGroup direction="vertical" class="h-full w-full">
+        <ResizablePanel>
+          <TypeGrid />
+        </ResizablePanel>
+        <ResizableHandle with-handle />
+        <ResizablePanel>
+          <TagGrid />
+        </ResizablePanel> </ResizablePanelGroup
+    ></ResizablePanel>
   </ResizablePanelGroup>
 </template>
 
 <script setup lang="ts">
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import MarksOnline from "@/components/device/MarkOnline.vue";
+import TypeGrid from "@/components/TypeGrid.vue";
+import TagGrid from "@/components/TagGrid.vue";
 import DeviceCreate from "@/components/DeviceCreateForm.vue";
 </script>
